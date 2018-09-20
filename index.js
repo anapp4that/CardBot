ea560d10f6db4b636cc52c7b9c2a621d543498db
anapp4that <sla008@uark.edu>
2018-09-19 23:24:27 -0500

    Pulling updated index.js

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const fs = require('fs');
 var botToken = fs.readFileSync('./token.txt', 'utf-8');
 botToken = botToken.toString();
 botToken = botToken.trim();
-console.log(botToken);
+//console.log(botToken);
 
 // create a bot
-var bot = new SlackBot({
+var cardBot = new SlackBot({
     token: botToken,
     name: 'CardBot'
 });
 
-bot.on('start', function() {
+cardBotot.on('start', function() {
     var params = {
         icon_emoji: ':female-judge:'
     };
 
-    bot.postMessage('test', 'test', params);
+    cardBot.postMessage('test', 'test', params);
 
 });
