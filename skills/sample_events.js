4e5fc9cb18ff7e30aacbac78a5261947975aac87
Will Poorman <will.k.poorman@gmail.com>
2018-10-28 17:28:01 -0500

    Add a few test events

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -3,20 +3,36 @@ module.exports = function(controller) {
     bot.reply(message, 'Welcome, <@' + message.user + '>');
   });
 
-  controller.on('app_mention', function(bot, message) {
-    console.log('Responding to mention with: You called?');
-    bot.reply(message, 'You called?');
-  });
+  // controller.on("app_mention", function(bot, message) {
+  //   console.log("Responding to mention with: You called?");
+  //   bot.reply(message, "You called?");
+  // });
 
   controller.on('direct_message', function(bot, message) {
     console.log('Responding to DM with: Hi, this is my DMs');
     bot.reply(message, 'Hi, this is my DMs');
   });
 
-  controller.on('ambient', function(bot, message) {
-    console.log(
-      'Responding to message in channel with: Oh hey, people are talking'
-    );
-    bot.reply(message, 'Oh hey, people are talking');
+  // controller.on("ambient", function(bot, message) {
+  //   console.log(
+  //     "Responding to message in channel with: Oh hey, people are talking"
+  //   );
+  //   bot.reply(message, "Oh hey, people are talking");
+  // });
+
+  // controller.hears("crap", "direct_message", function(bot, message) {
+  //   bot.reply(message, "Oh no, what'd you do this time?");
+  // });
+
+  // add a middleware to log messages sent
+  controller.middleware.send.use(function(bot, message, next) {
+    console.log('Sending: ', message);
+    next();
+  });
+
+  // add a middleware to log messages received
+  controller.middleware.receive.use(function(bot, message, next) {
+    console.log('Sending: ', message);
+    next();
   });
 };
